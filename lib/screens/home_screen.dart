b2b0b9afcc65c26244db55251d7717941994c658
Marlon Carrion <marlon.carrion.est@tecazuay.edu.ec>
2022-04-28 00:02:31 -0500

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,35 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:practice_two/screens/listview1_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Componentes de Flutter')),
-          elevation: 10,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Componentes de Flutter')),
+        elevation: 10,
+      ),
+      body: ListView.separated(
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+          title: const Text('Hell!'),
+          trailing: const Icon(Icons.accessibility_rounded),
+          onTap: () {
+            Navigator.pushNamed(context, "card");
+            //final route = MaterialPageRoute(
+            //    builder: (context) => const ListView1Screen());
+            //Navigator.push(context, route);
+            //pushReplacement sirve cuando no se necesita regresar a la pantalla anterior
+            //Navigator.pushReplacement(context, route);
+          },
         ),
-        body: ListView.separated(
-          itemCount: 5,
-          itemBuilder: (context, index) => ListTile(
-            title: const Text('Hell!'),
-            trailing: const Icon(Icons.accessibility_rounded),
-            onTap: () {
-              final route = MaterialPageRoute(
-                  builder: (context) => const ListView1Screen());
-              Navigator.push(context, route);
-              //pushReplacement sirve cuando no se necesita regresar a la pantalla anterior
-              //Navigator.pushReplacement(context, route);
-            },
-          ),
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ),
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
   }
